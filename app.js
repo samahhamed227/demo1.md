70c86d391c17e196565bad76c2bb190630c51465
samahhamed227 <samahhamed227@gmail.com>
2021-06-20 16:35:07 +0300

    new

---
+++ app.js
@@ -0,0 +1,41 @@
+
+  
+let user1 = prompt(' whats your name?');
+console.log(user1);
+alert ("welcome "+user1);
+
+
+  
+let user2 = prompt(' whats your age?');
+console.log(user2);
+alert ("I wish you a happy new year at  "+user2);
+
+  
+let user3 = prompt(' whats your favourit day?');
+console.log(user3);
+alert (user3 +"it\'s a good day");
+
+ let qa = prompt('Are you Ok? ');
+ console.log(qa);
+ alert ("I wish you a nice day now");
+
+
+
+
+/// const
+
+ const ID = '1236789';
+ console.log(ID);
+
+
+
+
+
+
+
+
+
+
+
+
+
